354e93326af68d5db6b7301faad9b2e2c1a99138
Andrey <xamidylin@gismeteo.com>
2018-12-21 12:07:10 +0300

    f

--- common/index.ts
+++ common/index.ts
@@ -1,10 +1,13 @@
 import {Logger} from "./logger";
 import {ApiService, ApiUrlInjectionToken} from "./api.service";
 import {IRequestService} from "./i-request.service";
+import {Container} from "@so/di";
 
 export * from './logger';
 export * from './api.service';
 export * from './i-request.service';
+
+export const InfrContainer = new Container();
 
 export const CommonProviders = [
     {provide: Logger},
